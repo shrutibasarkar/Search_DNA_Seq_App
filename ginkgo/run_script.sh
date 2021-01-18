9a5cc9fb2f9b4145155542a71c42648bd71e9dc8
Shruti <shrutibasarkaracea@gmail.com>
2021-01-18 18:14:52 -0500

    readme file and run script added

---
+++ ginkgo/run_script.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo "running script......"
+
+# cd into frontend directory
+cd DNA_seq_search/frontend
+
+# npm install
+echo "installing npm packages......"
+npm install
+
+# cd out one directory
+cd ../
+
+# run migration script
+echo "running migrations....."
+python manage.py migrate 
+python manage.py makemigrations
+
+# open app locally on browser 
+# this command runs on mac system 
+# for windows run 
+# start http://127.0.0.1:8000
+open http://127.0.0.1:8000
+
+# run server 
+python manage.py runserver  
+
+
+
+
+
+
+
+
+# 2. cd to DNA_seq_search/  run following command
+		# python manage.py migrate         #get database data
+		# python manage.py makemigrations
+        # run server        
+		# python manage.py runserver    
+
+
+ #    NOde    modules
