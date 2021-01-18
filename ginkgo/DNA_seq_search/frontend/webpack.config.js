e4db8a08f39a59bd0554a78d606b9b7f3c3103bf
Shruti <shrutibasarkaracea@gmail.com>
2021-01-18 10:22:04 -0500

    add more files

--- ginkgo/DNA_seq_search/frontend/webpack.config.js
+++ ginkgo/DNA_seq_search/frontend/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./static/javascript"),
+    path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
   },
   module: {
