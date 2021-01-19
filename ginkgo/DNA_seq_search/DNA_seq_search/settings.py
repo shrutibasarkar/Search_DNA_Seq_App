c54e8389063d2390635781bbdd5e88b1b44fa35f
Shruti <shrutibasarkaracea@gmail.com>
2021-01-18 19:10:46 -0500

    debug false

--- ginkgo/DNA_seq_search/DNA_seq_search/settings.py
+++ ginkgo/DNA_seq_search/DNA_seq_search/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p5va#a!grii8tc5l0=b)k3rys3ean^gh8o^d^cszsb88+e0^p)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
